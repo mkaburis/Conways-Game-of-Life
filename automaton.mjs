84016d2b4fffdf67cb70f7088f9267f2afb60fbe
Mihail Kaburis <mkaburis@gmail.com>
2019-11-16 12:26:30 -0500

    Fixed UNIT const issue

--- automaton.mjs
+++ automaton.mjs
@@ -1,63 +1,4 @@
-
-const UNIT = 18; // each unit in the world is 18 pixels
-const N = 50; // size of grid
-const ALIVE = 1
-const DEAD = 0
-let stateArray // a 2D array with the current states of the grid
-let nextArray // 2D array which will hold next state of grid
-let isRunning = false;
-
-function setup() {
-    canvas = createCanvas(1100, 500);
-    canvas.parent('canvas-container')
-    // centerCanvas();
-
-    stateArray = createArray(N, N);
-    nextArray = createArray(N, N);
-
-    documentReady();
-
-    initRandom();
-    //console.log(stateArray);
-}
-
-function draw() {
-    for (let y = 0; y < N; y++) {
-        for (let x = 0; x < N; x++) {
-            let xloc = x * UNIT;
-            let yloc = y * UNIT;
-
-            if (stateArray[x][y] == ALIVE) {
-                fill(0, 103, 71)
-                stroke(0, 103, 71)
-                rect(xloc, yloc, UNIT, UNIT)
-            }
-            else {
-                fill(255)
-                stroke(0, 103, 71)
-                rect(xloc, yloc, UNIT, UNIT)
-            }
-
-        }
-    }
-}
-
-function createArray(colums, rows) {
-    let array = new Array(colums)
-    for (let i = 0; i < array.length; i++) {
-        // set all values of the array in 0
-        array[i] = new Array(rows).fill(0);
-    }
-    return array;
-}
-
-function initRandom() {
-    for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N; j++) {
-            stateArray[i][j] = floor(random(2))
-        }
-    }
-}
+export{documentReady}
 
 function conwayCellLogic(x, y) {
     neighborCount = 0;
@@ -222,59 +163,12 @@ function mousePressed() {
     let x_box;
     let y_box;
 
-    x_box = floor(mouseX / UNIT);
-    y_box = floor(mouseY / UNIT);
+    x_box = floor(mouseX / N);
+    y_box = floor(mouseY / N);
 
     stateArray[x_box][y_box] = 1;
 }
 
-function calculateNeighbors(x, y)  //Takes in x, y of cell and checks all neighbors
-{
-    let neighborCount = 0;
-    
-    for(let i = -1; i < 2; i++)
-    {
-       for(let j = -1; j < 2; j++)
-       {
-           let col = (x + i + cols) % cols;
-           let row = (y + j + rows) % rows;
-           neighborCount += 1;
-       }
-    }
-    neighborCount -= [x][y];  //Remove count of itself becasue not included in neighbors
-    return neighborCount;
-}
-
-function calculateNextBoard()
-{
-    let newBoard = createArray(N, N); //cols, rows
-    
-    for(let i = 0; i < N; i++)
-    {
-        for(let j = 0; j < N ; j++)
-        {
-             let currentState = stateArray[i][j];
-            
-            let val = 0;
-            let neighborCount = calculateNeighbors(i, j);
-            
-            if(currentState == Dead && neighborCount == 3)
-            {
-               newBoard[i][j] = Alive;
-            }
-            else if(currentState == Alive && (neighborCount < 2 || neighborCount > 3))
-            {
-                newBoard[i][j] = Dead;
-            }
-            else
-            {
-                newBoard[i][j] = currentState;
-            }
-        }
-    }
-    
-    stateArray = newBoard;
-}
 
 function documentReady() {
     let startBtn = document.getElementById('startBtn');
