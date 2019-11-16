eda9e2fdc6bf615c86be2edd5fc2a92f8e3985dd
Jose-Pablo <runing456@gmail.com>
2019-11-15 19:52:46 -0500

    formatting changes

--- sketch.js
+++ sketch.js
@@ -17,105 +17,98 @@ function setup() {
     documentReady();
 
     initRandom();
-    console.log(stateArray);
+    //console.log(stateArray);
 }
 
-
 function draw() {
-    for(let y = 0; y < N; y++) {
-        for(let x = 0; x < N; x++) {
+    for (let y = 0; y < N; y++) {
+        for (let x = 0; x < N; x++) {
             let xloc = x * UNIT;
             let yloc = y * UNIT;
-            
-            if(stateArray[x][y] == ALIVE)
-            {
+
+            if (stateArray[x][y] == ALIVE) {
                 fill(0, 103, 71)
                 stroke(0, 103, 71)
                 rect(xloc, yloc, UNIT, UNIT)
             }
-            else
-            {
+            else {
                 fill(255)
                 stroke(0, 103, 71)
                 rect(xloc, yloc, UNIT, UNIT)
             }
-            
+
         }
     }
 }
 
-function createArray(colums, rows)
-{
+function createArray(colums, rows) {
     let array = new Array(colums)
-    for(let i = 0; i < array.length; i++)
-    {
+    for (let i = 0; i < array.length; i++) {
         // set all values of the array in 0
         array[i] = new Array(rows).fill(0);
     }
     return array;
 }
 
-function initRandom()
-{
-    for(let i = 0; i < N; i++) {
-        for(let j = 0; j < N; j++) {
+function initRandom() {
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
             stateArray[i][j] = floor(random(2))
         }
     }
 }
 
-function conwayCellLogic (x, y)
-{
+function conwayCellLogic(x, y) {
     neighborCount = 0;
     isLive = true;
 
     // count cell neighbors
     // topleft
-    if (x-1 >= 0 && y-1 >= 0) // not out of bounds
+    if (x - 1 >= 0 && y - 1 >= 0) // not out of bounds
     {
-        if (stateArray[x-1][y-1] == ALIVE) // Cell lives?
+        if (stateArray[x - 1][y - 1] == ALIVE) // Cell lives?
             neighborCount++;
     }
     // topCenter
-    if (y-1 >= 0) // not out of bounds
+    if (y - 1 >= 0) // not out of bounds
     {
-        if (statArray[x][y-1] == ALIVE) // cell lives?
+        if (statArray[x][y - 1] == ALIVE) // cell lives?
             neighborCount++
     }
     // top right
-    if (x+1 <= N && y-1 >= 0) // not out of bounds
+    if (x + 1 <= N && y - 1 >= 0) // not out of bounds
     {
-        if (statArray[x+1][y-1] == ALIVE) // cell lives?
+        if (statArray[x + 1][y - 1] == ALIVE) // cell lives?
             neighborCount++
     }
     // left
-    if (x-1 >= 0) // not out of bounds
+    if (x - 1 >= 0) // not out of bounds
     {
-        if (statArray[x-1][y] == ALIVE) // cell lives?
+        if (statArray[x - 1][y] == ALIVE) // cell lives?
             neighborCount++
     }
     // right
-    if (x+1 <= N) // not out of bounds
+    if (x + 1 <= N) // not out of bounds
     {
-        if (statArray[x+1][y] == ALIVE) // cell lives?
+        if (statArray[x + 1][y] == ALIVE) // cell lives?
             neighborCount++
     }
     // bottom left
-    if (x-1 >= 0 && y+1 <= N) // not out of bounds
+    if (x - 1 >= 0 && y + 1 <= N) // not out of bounds
     {
-        if (statArray[x-1][y+1] == ALIVE) // cell lives?
+        if (statArray[x - 1][y + 1] == ALIVE) // cell lives?
             neighborCount++
     }
     // bottom center
-    if (y+1 <= N) // not out of bounds
+    if (y + 1 <= N) // not out of bounds
     {
-        if (statArray[x][y+1] == ALIVE) // cell lives?
+        if (statArray[x][y + 1] == ALIVE) // cell lives?
             neighborCount++
     }
     // bottom right
-    if (x+1 <= N && y+1 <= N) // not out of bounds
+    if (x + 1 <= N && y + 1 <= N) // not out of bounds
     {
-        if (statArray[x+1][y+1] == ALIVE) // cell lives?
+        if (statArray[x + 1][y + 1] == ALIVE) // cell lives?
             neighborCount++
     }
 
@@ -125,8 +118,8 @@ function conwayCellLogic (x, y)
      *  if cell should live, set live *
      **********************************/
     // if cell is dead, set tracker dead, need for logic below
-    if (!stateArray[x][y])  
-        isLive = false; 
+    if (!stateArray[x][y])
+        isLive = false;
 
     // if neighbors < 2 or > 3, DIE
     if (neighborCount < 2 || neighborCount > 3)
@@ -139,13 +132,12 @@ function conwayCellLogic (x, y)
         return true;
 }
 
-function initSpaceship()
-{
-    let mid_x = floor(N/2);
-    let mid_y = floor(N/2);
-    
-    stateArray[mid_x - 2][mid_y -2] = 1;
-    stateArray[mid_x - 1][mid_y -2] = 1;
+function initSpaceship() {
+    let mid_x = floor(N / 2);
+    let mid_y = floor(N / 2);
+
+    stateArray[mid_x - 2][mid_y - 2] = 1;
+    stateArray[mid_x - 1][mid_y - 2] = 1;
     stateArray[mid_x][mid_y - 2] = 1;
     stateArray[mid_x - 3][mid_y - 1] = 1;
     stateArray[mid_x - 2][mid_y - 1] = 1;
@@ -159,96 +151,93 @@ function initSpaceship()
     stateArray[mid_x + 2][mid_y] = 1;
     stateArray[mid_x][mid_y + 1] = 1;
     stateArray[mid_x + 1][mid_y + 1] = 1;
-    
+
 }
 
-function initBlinker()
-{
-     let mid_x = floor(N/2); 
-     let mid_y = floor(N/2);
-    
+function initBlinker() {
+    let mid_x = floor(N / 2);
+    let mid_y = floor(N / 2);
+
     stateArray[mid_x + 2][mid_y - 1] = 1;
     stateArray[mid_x + 2][mid_y] = 1;
     stateArray[mid_x + 2][mid_y + 1] = 1;
-    
+
     stateArray[mid_x - 2][mid_y - 1] = 1;
     stateArray[mid_x - 2][mid_y] = 1;
     stateArray[mid_x - 2][mid_y + 1] = 1;
-    
-    
+
+
 }
 
-function initPulsar()
-{
-     let mid_x = floor(N/2); 
-     let mid_y = floor(N/2);
-    
+function initPulsar() {
+    let mid_x = floor(N / 2);
+    let mid_y = floor(N / 2);
+
     stateArray[mid_x - 1][mid_y - 2] = 1;
     stateArray[mid_x][mid_y - 2] = 1;
     stateArray[mid_x + 1][mid_y - 2] = 1;
-    
+
     stateArray[mid_x - 2][mid_y - 1] = 1;
     stateArray[mid_x][mid_y - 1] = 1;
     stateArray[mid_x + 2][mid_y - 1] = 1;
-    
+
     stateArray[mid_x - 3][mid_y] = 1;
     stateArray[mid_x - 2][mid_y] = 1;
     stateArray[mid_x - 1][mid_y] = 1;
     stateArray[mid_x + 1][mid_y] = 1;
     stateArray[mid_x + 2][mid_y] = 1;
-    
+
     stateArray[mid_x - 3][mid_y + 1] = 1;
     stateArray[mid_x][mid_y + 1] = 1;
     stateArray[mid_x + 2][mid_y + 1] = 1;
-    
+
     stateArray[mid_x][mid_y + 2] = 1;
     stateArray[mid_x + 1][mid_y + 2] = 1;
-    
+
     stateArray[mid_x - 1][mid_y + 3] = 1;
     stateArray[mid_x][mid_y + 3] = 1;
 }
 
-function initBlank(){
-    for(let i = 0; i < N; i++) {
-        for(let j = 0; j < N; j++) {
+function initBlank() {
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
             stateArray[i][j] = 0;
         }
     }
 }
 
-function mousePressed()
-{
+function mousePressed() {
     let x_box;
     let y_box;
-    
-    x_box = floor(mouseX/N);
-    y_box = floor(mouseY/N);
-    
+
+    x_box = floor(mouseX / N);
+    y_box = floor(mouseY / N);
+
     stateArray[x_box][y_box] = 1;
 }
 
 
-function documentReady(){
+function documentReady() {
     let startBtn = document.getElementById('startBtn');
     let stopBtn = document.getElementById('stopBtn');
     let stepBtn = document.getElementById('stepBtn');
     let listOfConfigs = document.getElementById('listOfConfigs');
 
     const configs = {
-        'Clear' : initBlank,
-        'Random' : initRandom,
-        'SpaceShip' : initSpaceship,
-        'Blinker' : initBlinker,
-        'Pulsar' : initPulsar
+        'Clear': initBlank,
+        'Random': initRandom,
+        'SpaceShip': initSpaceship,
+        'Blinker': initBlinker,
+        'Pulsar': initPulsar
     }
 
     listOfConfigs.innerHTML = '';
-    
+
     for (const key in configs) {
 
         let option = document.createElement('option');
-        option.innerText = key;     
-        listOfConfigs.append(option);       
+        option.innerText = key;
+        listOfConfigs.append(option);
     }
 
     startBtn.addEventListener('click', () => {
@@ -265,6 +254,7 @@ function documentReady(){
     })
 
     listOfConfigs.addEventListener('change', (x, y) => {
+        isRunning = false;
         let index = listOfConfigs.selectedIndex;
         let key = listOfConfigs[index].innerHTML;
 
